a370a4b65945f0d1022361ddc400fd5425f149c3
Navid shad <navidshad72@gmail.com>
2017-11-30 22:14:13 +0330

    improve 'did you mean'

--- moduls/google_translate.js
+++ moduls/google_translate.js
@@ -3,9 +3,15 @@ var googletrans = require('google-translate-api');
 var get = function(text, from, to, callback){
     googletrans(text, {'from': from, 'to': to}).then(res => {
         var result = '';
-        if(res.from.text.didYouMean) result = res.from.text.value;
-        else result = res.text;
-        if(callback) callback(result);
+        if(res.from.text.didYouMean) {
+            get(res.from.text.value, from, to, (r) => {
+                if(callback) callback(r);
+            });
+        }
+        else {
+            result = res.text;
+            if(callback) callback(result);
+        }
         //console.log(res.text);
         //=> Ik spreek Nederlands! 
         //console.log(res.from.text.autoCorrected);
