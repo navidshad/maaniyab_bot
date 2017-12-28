dc37fd010e41e36dcaa6ea421c08331cad50f541
Navid shad <navidshad72@gmail.com>
2017-12-28 20:52:25 +0330

    debug

--- confige.js
+++ confige.js
@@ -1,7 +1,7 @@
 module.exports = {
     //bot
-    dbpath      :'mongodb://127.0.0.1:27017/dictionary',//'mongodb://maaniyab:maaniyab131416@ds123926.mlab.com:23926/maaniyab_bot',
-    token       :'357787314:AAF6K-Tb7VnKM_SLZOUwCIa0zLaREp730r4',//'461784810:AAFZ2d04mFXXh7GSBfVSx7VOY2XmKOWVjhg',
+    dbpath      :'mongodb://maaniyab:maaniyab131416@ds123926.mlab.com:23926/maaniyab_bot',
+    token       :'461784810:AAFZ2d04mFXXh7GSBfVSx7VOY2XmKOWVjhg',
     botusername :'maaniyab_bot',
     modules:{
         'settings'          :true,
