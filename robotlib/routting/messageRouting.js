1876893a5f734ded5c6c8d2579563cd6e6fb8b99
Navid <navidshad72@gmail.com>
2018-01-02 09:54:37 +0330

    Update messageRouting.js
    debug

--- robotlib/routting/messageRouting.js
+++ robotlib/routting/messageRouting.js
@@ -16,7 +16,8 @@ var analyze = function(message){
             
             //sperate section
             var speratedSection = '';
-            if(user && user.section) speratedSection = user.section.split('/');
+            if(!user) { global.robot.bot.sendMessage(message.from.id, "لطفا دستر زیر را به ربات ارسال کن." + "\n" + "/start"); return;}
+            else(user.section) speratedSection = user.section.split('/');
             
             //go to meain menu
             if(message.text && message.text === fn.str['backToMenu']) fn.commands.backToMainMenu(message, user);
