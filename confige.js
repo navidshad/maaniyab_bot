379fd04b0f9e05b32697eea768f6e30caf8d6167
Navid <navidshad72@gmail.com>
2018-09-26 11:01:42 +0330

    Update confige.js

--- confige.js
+++ confige.js
@@ -1,7 +1,7 @@
 module.exports = {
     //bot
-    dbpath      :'mongodb://maaniyab:maaniyab131416@ds123926.mlab.com:23926/maaniyab_bot',
-    token       :'461784810:AAFZ2d04mFXXh7GSBfVSx7VOY2XmKOWVjhg',
+    dbpath      :'',
+    token       :'',
     botusername :'maaniyab_bot',
     modules:{
         'settings'          :true,
@@ -15,14 +15,4 @@ module.exports = {
     //web
     serverport:90,
     //token       :'357787314:AAF6K-Tb7VnKM_SLZOUwCIa0zLaREp730r4', //test
-
-    //dictionary apis
-    urbanDictionaryAPI  : 'https://mashape-community-urban-dictionary.p.mashape.com/define',
-    urbanDictionaryKey  : 'eEhZLtSBxvmsh5kusOiCRsnXhG4xp1yAAtKjsnGlE9FUIHQcWx',
-
-    //botanio statistic
-    yandexKey           :'18681d5a-50a2-485b-a43d-efb1a78d0e9e',
-
-    //google analytic
-    g_trackingID : 'UA-113123995-1',
 }
